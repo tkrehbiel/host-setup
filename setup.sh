ce3290ee808f8041a79c404fa10220ba5d74904a
Endgame Viable <endgameviable@gmail.com>
2022-11-09 09:55:10 -0500

    conditional authorized_keys, nginx

--- setup.sh
+++ setup.sh
@@ -10,11 +10,24 @@ username=$(whoami)
 echo "Current user is $username"
 
 mkdir -p ~/.ssh
-echo "Enter public key for login:"
-read pubkey
-echo $pubkey >~/.ssh/authorized_keys
-chown $username:$username ~/.ssh/authorized_keys
-chmod 600 ~/.ssh/authorized_keys
+if [ ! -f ~/.ssh/authorized_keys ]; then
+    echo "Enter public key for login:"
+    read pubkey
+    echo $pubkey >~/.ssh/authorized_keys
+    chown $username:$username ~/.ssh/authorized_keys
+    chmod 600 ~/.ssh/authorized_keys
+fi
+
+# allow ssh through ubuntu firewall!
+sudo ufw allow ssh
 
 echo "Reminder: Don't forget to disable root and password login"
+
+sudo apt update
+sudo apt upgrade
+sudo apt install nginx
+sudo ufw allow 443
+sudo ufw allow 8080
+
+echo "Reminder: Enable ufw with sudo ufw enable"
 
