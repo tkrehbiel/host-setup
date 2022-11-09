76928a393de27e6360326781128c3e03dc2bb651
Endgame Viable <endgameviable@gmail.com>
2022-11-09 18:31:53 -0500

    nginx and certbot

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-# Setup Ubuntu host
+# Setup Ubuntu host from scratch
 
 # First login as root and create a new user with sudo privileges
 # - adduser name
@@ -9,6 +9,13 @@
 username=$(whoami)
 echo "Current user is $username"
 
+sudo apt update
+sudo apt upgrade
+
+# allow ssh through ubuntu firewall!
+sudo ufw allow ssh
+
+# setup ssh private key access
 mkdir -p ~/.ssh
 if [ ! -f ~/.ssh/authorized_keys ]; then
     echo "Enter public key for login:"
@@ -18,16 +25,25 @@ if [ ! -f ~/.ssh/authorized_keys ]; then
     chmod 600 ~/.ssh/authorized_keys
 fi
 
-# allow ssh through ubuntu firewall!
-sudo ufw allow ssh
-
-echo "Reminder: Don't forget to disable root and password login"
-
-sudo apt update
-sudo apt upgrade
+# install nginx
 sudo apt install nginx
 sudo ufw allow 443
 sudo ufw allow 8080
 
-echo "Reminder: Enable ufw with sudo ufw enable"
+# install certbot
+sudo apt install snapd
+sudo snap install core; sudo snap refresh core
+sudo snap install --classic certbot
+sudo ln -s /snap/bin/certbot /usr/bin/certbot
 
+if [ ! -f /etc/letsencrypt/live/$uvtek_hostname/cert.pem ]; then
+    sudo certbot --nginx
+fi
+
+# nginx config
+cp nginx/sites-available/default /etc/nginx/sites-available
+sudo systemctl restart nginx
+
+echo "*** Reminders ***"
+echo "Disable root and password login: /etc/ssh/sshd_config"
+echo "Enable ufw: sudo ufw enable"
