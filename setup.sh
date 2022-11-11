2cd0f1542d9e55b5b488d525c4a44f2ef94cb0a7
Endgame Viable <endgameviable@gmail.com>
2022-11-10 20:57:48 -0500

    remove noip

--- setup.sh
+++ setup.sh
@@ -75,7 +75,7 @@ rm -f sub.txt
 sudo systemctl restart nginx
 
 # setup remark42
-sudo apt install noip2 docker-compose
+sudo apt install docker-compose
 envsubst <remark42/docker-compose.template >remark42/docker-compose.yml
 echo "To Start remark42 -- cd remark42; ./restart.sh"
 
